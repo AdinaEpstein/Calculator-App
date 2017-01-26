640eb7e92ef2e6327b47b0ec37220289224ce293
Adina Epstein <Adina-Epstein@Adinas-MacBook-Pro.local>
2017-01-26 13:49:22 -0800

    jDoc comments added

--- calculator_start.js
+++ calculator_start.js
@@ -20,6 +20,7 @@ function displayCurrentInput() {
     document.getElementById('screen').value = currentInput;
 }
 // Adds a digit to the current input
+
 function addDigit(dig) {
     if (currentInput.length + 1 > 18) {
         alert("No more than 18 characters");
@@ -46,7 +47,10 @@ function addDecimal() {
     }
     displayCurrentInput();
 }
-// Clears everything.
+
+/**
+ * Clears everything
+ */
 function allClear() {
     currentInput = "";
     console.log("Current input cleared");
@@ -56,7 +60,11 @@ function allClear() {
     console.log("Memory cleared");
     displayCurrentInput();
 }
-// Stores the last operator pushed for multiply, divide, add, or subtract.
+
+/**
+ * Stores the last operator pushed for multiply, divide, add or subtract
+ * @param {string} op passes string from the html to store operator
+ */
 function storeOperator(op) {
     if (op.indexOf("*") > -1) {
         operator = 1;
@@ -81,7 +89,10 @@ function storeOperator(op) {
     console.log(operator);
     console.log(memory);
 }
-// Calculate using operator, the memory and what is current
+
+/**
+ * Calculate using the operator, the memery and the current input
+ */
 function calculate() {
     if (operator == 1) {
         currentInput = eval(memory) * eval(currentInput);
@@ -124,19 +135,28 @@ function calculate() {
     memory = "0"; //clear memory
     displayCurrentInput();
 }
-// Change the sign of the current input
+
+/**
+ * Changes the sign of the current input
+ */
 function changeSign() {
     currentInput = -1 * currentInput;
     displayCurrentInput();
 }
-// Clear the current input back to 0
+
+/**
+ * Resets the current input to 0
+ */
 function pleaseClear() {
     currentInput = "0";
     console.log("Current input cleared");
     console.log("Operator remains " + operator + " and memory remains " + memory);
     displayCurrentInput();
 }
-// Change the current input to a percentage
+
+/**
+ * Converts the current input into a percentage
+ */
 function percentage() {
     if (currentInput.indexOf("%") == -1) {
         currentInput = currentInput * 100;
@@ -144,7 +164,10 @@ function percentage() {
     }
     displayCurrentInput();
 }
-// Calculate the factorial of the current input
+
+/**
+ * Calculates the factorial of the current input
+ */
 function factorial() {
     var i = "";
     var fact = 1;
@@ -159,12 +182,18 @@ function factorial() {
     }
     displayCurrentInput();
 }
-// Calculate the square of the current input
+
+/**
+ * Squares the current input
+ */
 function square() {
     currentInput = currentInput * currentInput;
     displayCurrentInput();
 }
-// Calculate the square root of the current input
+
+/**
+ * Calculates the square root of the current input
+ */
 function squareRoot() {
     if (currentInput == -1) {
         currentInput = "i";
@@ -178,50 +207,84 @@ function squareRoot() {
     }
     displayCurrentInput();
 }
-// Calculate the inverse of the current input
+
+/**
+ * Calculates the inverse of the current input
+ */
 function inverse() {
     currentInput = 1 / currentInput;
     displayCurrentInput();
 }
-//trig functions:
+
+/**
+ * Takes the sine of the current input
+ */
 function sin() {
     currentInput = Math.sin(currentInput);
     displayCurrentInput();
 }
 
+/**
+ * Takes the cosine of the current input
+ */
 function cos() {
     currentInput = Math.cos(currentInput);
     displayCurrentInput();
 }
 
+/**
+ * Takes the tangent of the current input
+ */
 function tan() {
     currentInput = Math.tan(currentInput);
     displayCurrentInput();
 }
 
+/**
+ * Changes the input from degrees to radians
+ */
 function toRadian() {
     currentInput = currentInput * (Math.PI / 180);
     displayCurrentInput();
     console.log("working");
 }
 
+/**
+ * Changes the input from radians to degrees
+ */
 function toDegree() {
     currentInput = currentInput * (180 / Math.PI);
     displayCurrentInput();
 }
+
+/**
+ * Saves the memory as the current input
+ */
 function ms() {
     otherMemory = currentInput;
     console.log(otherMemory);
 }
+
+/**
+ * Recalls the saved memory
+ */
 function mr() {
     currentInput = otherMemory;
     displayCurrentInput();
     console.log("memory is " + otherMemory);
 }
+
+/**
+ * Adds the current calc input from the stored memory
+ */
 function mplus() {
     otherMemory = eval(otherMemory) + eval(currentInput);
     console.log("memory is " + otherMemory);
 }
+
+/**
+ * Subtracts the current calc input from the stored memory
+ */
 function mminus() {
     otherMemory = eval(otherMemory) - eval(currentInput);
 }
